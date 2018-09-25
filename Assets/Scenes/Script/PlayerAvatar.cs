f4958def702e5136c6f3b3b5ddf1dce69849a98f
louis <lpdt@live.fr>
2018-09-25 17:47:07 +0200

    starting project

---
+++ Assets/Scenes/Script/PlayerAvatar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAvatar : MonoBehaviour {
+
+	public float jumpImpulse;
+    public float speed;
+    public float maxDownSpeed;
+    public float downSpeedPerSec;
+    private float nearObjectUp;
+    private float nearObjectDown;
+    private float nearObjectRight;
+    private float nearObjectLeft;
+
+    private Vector2 position;
+    void Start () {
+        position = gameObject.transform.position;
+	}
+	
+    void Jump ()
+    {
+
+    }
+
+    void Mouvement()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
